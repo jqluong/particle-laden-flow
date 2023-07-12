03460e220f9ebffe15ff27f66433c03b581e23df
jqluong <jackaham_luongcoln@mail.fresnostate.edu>
2023-07-11 17:14:41 -0700

    Example of changing the angle + flux table file for simulations
    To run a simulation:
    -go into bidisperse_PDE_test
    -change ang
    -change file name in call to bidisperse_PDE_exp (all flux files should already be in directory)

--- bidisperse_pde/bidisperse_PDE_test.m
+++ bidisperse_pde/bidisperse_PDE_test.m
@@ -5,11 +5,11 @@ addpath(parentDirectory + "/ODEsolve")
 
 
 %Set Constants
-ang = 30;
+ang = 25;
 A = set_constants2(ang); %This is where you change the angle
 
 phi0 = 0.5;
-X0 = 0.2;
+X0 = 0.7;
 
 tf = 20;
 nout = 41;
@@ -22,4 +22,4 @@ L = [20 20];
 figure(1)
 figure(2) %These need to exist for the solver to output plots per step.
 
-sol = bidisperse_PDE_exp(ang,phi0,X0,tf,t_out,vol,L,"ftableBD_30",1)
+sol = bidisperse_PDE_exp(ang,phi0,X0,tf,t_out,vol,L,"ftableBD_25",1)

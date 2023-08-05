f27de00abf833ada3ff74cbab7578024283bb349
jqluong <jackaham_luongcoln@mail.fresnostate.edu>
2023-08-04 17:49:58 -0700

    Cleaned up flux tables to different folder
    -Moved flux tables to new folders for GSB3/GSB9
    -Changed diameters for new experiment

--- ODEsolve/set_constants2.m
+++ ODEsolve/set_constants2.m
@@ -20,8 +20,8 @@ if(nargin > 1 && ~isempty(varargin{2}))
     rholiq = varargin{4}; %density of liquid
     rho = varargin{5};  %density of sphere
 else   
-    d1 = 725 * 10^-6;  % diameter of larger particle (m)
-    d2 = 152.5 * 10^-6; % diameter of smaller particle
+    d1 = 325 * 10^-6;  % diameter of larger particle (m)
+    d2 = 225 * 10^-6; % diameter of smaller particle
     rholiq = 971;    % density of the liquid (kg/m^3)
     rho = 2475; %density of the sphere
 end
--- bidisperse_pde/bidisperse_PDE_test.m
+++ bidisperse_pde/bidisperse_PDE_test.m
@@ -5,7 +5,7 @@ addpath(parentDirectory + "/ODEsolve")
 
 
 %Set Constants
-ang = 50;
+ang = 40;
 A = set_constants2(ang); %This is where you change the angle
 
 phi0 = 0.5;
@@ -22,4 +22,4 @@ L = [20 20];
 figure(1)
 figure(2) %These need to exist for the solver to output plots per step.
 
-sol = bidisperse_PDE_exp(ang,phi0,X0,tf,t_out,vol,L,"ftableBD_50",1)
+sol = bidisperse_PDE_exp(ang,phi0,X0,tf,t_out,vol,L,"ftableBD_40",1)
